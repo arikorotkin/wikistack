9b093e53d2fbdeb2a4b4fe15a75fe02b3281d954
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2020-02-09 14:14:02 -0600

    created some tables

--- models/index.js
+++ models/index.js
@@ -1,6 +1,35 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
-module.exports = {
-    db
-}
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false // name MUST have a value
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+})
+
+const Page = db.define('page' {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content:{
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM('open','closed'),
+    allowNull: false,
+    defaultValue: 'closed'
+  }
+})
+
+module.exports = { Page, User};
