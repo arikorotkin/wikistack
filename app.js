aaa15b4bc71ef5a64d56361e3e56d257a54f8ef5
amkdr <korotkin.ari@gmail.com>
2020-02-09 13:58:28 -0600

    connected to db

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
+const layout = require('./views/layout.js');
+const { db } = require('./models');
 
-app.get('/', (req, res) => console.log('Hello world!'));
+app.get('/', (req, res) => {
+    res.send(layout('My Wikipedia Article'));
+});
+
+db.authenticate()
+    .then(() => {
+        console.log('connected to the database');
+    })
 
 const PORT = 1234;
 
